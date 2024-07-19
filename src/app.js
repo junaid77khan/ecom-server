93e1a3d1fe6c24522f341b87007375c300e2d3da
Junaid Khan <junaidk8185@gmail.com>
2024-07-19 11:05:17 +0530

    Changing origin

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cookieParser from 'cookie-parser';
 const app = express()
 
 const corsConfig = {
-    origin: 'https://ecom-candle-decor.vercel.app/',
+    origin: 'https://ecom-candle-decor.vercel.app',
     credentials: true,
     methods: ["GET", "POST", "DELETE", "PUT"]
 }; 
