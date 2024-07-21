7a50d11450f223b46310d2af12d892523dfe96a0
Siddhant <siddhant_010@outlook.com>
2024-07-21 18:01:47 +0530

    otp

--- src/app.js
+++ src/app.js
@@ -1,41 +1,44 @@
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser';
-const app = express()
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import { config } from "dotenv";
+import paymentRoute from "./routes/PaymentRoute.js";
+const app = express();
+console.log("cors");
 
 const corsConfig = {
-<<<<<<< HEAD
-    // origin: `${process.env.CORS_ORIGIN}`,
-
-    origin: 'http://localhost:7000',
-
-=======
-    origin: 'https://ecom-candle-decor.vercel.app',
->>>>>>> 8ade00af9cbdd11b2d416043acc3f189cda8b243
-    credentials: true,
-    methods: ["GET", "POST", "DELETE", "PUT"]
-}; 
+  origin: "http://localhost:7000",
+  credentials: true,
+  methods: ["GET", "POST", "DELETE", "PUT"],
+};
+console.log("cors1");
 
 app.use(cors(corsConfig));
 
 // accept json format data
-app.use(express.json({limit: "50000kb"}))
+app.use(express.json({ limit: "50000kb" }));
 
 // url encoding
-app.use(express.urlencoded({extended: true, limit: "50000kb"}))
+app.use(express.urlencoded({ extended: true, limit: "50000kb" }));
 
 // for files at server
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use("/api", paymentRoute);
 
-import categoryRouter from "./routes/category.route.js"
-import ProductRouter from './routes/product.route.js';
-import userRouter from './routes/user.route.js';
+import categoryRouter from "./routes/category.route.js";
+import ProductRouter from "./routes/product.route.js";
+import userRouter from "./routes/user.route.js";
 import cartRouter from "./routes/cart.route.js";
+
+app.get("/api/getkey", (req, res) =>
+  res.status(200).json({ key: 'rzp_test_BmxEZzZl9QMupi' })
+);
 
 app.use("/api/v1/category", categoryRouter);
 app.use("/api/v1/product", ProductRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/cart", cartRouter);
 
-export default app
+export default app;
